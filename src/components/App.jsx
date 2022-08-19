53ff50fa8b44cba4c770a12e33baaafc6dc3821d
iali <engr.imran_1242@hotmail.com>
2022-08-19 15:45:58 +0200

    final phase working

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,14 +1,18 @@
-import React from "react";
-import Form from "./Form";
+import React from 'react'
+import Header from './Header'
+import Footer from './Footer'
+import Note from './Note'
+import CreateArea from './CreateArea'
 
-const userIsRegistered = false;
-
-const App = () => (
-    <div className="container">
-        <Form
-            isRegister={userIsRegistered}
-        />
+function App() {
+  return (
+    <div>
+      <Header />
+      <CreateArea />
+      <Note key={1} title="Note title" content="Note content" />
+      <Footer />
     </div>
-)
+  )
+}
 
-export default App;
+export default App
