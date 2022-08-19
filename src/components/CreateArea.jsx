6f7ae1c4f5e4c58f047e76ed6188d5d21ac451e7
iali <engr.imran_1242@hotmail.com>
2022-08-19 15:59:07 +0200

    working on title and subtitle first phase

--- src/components/CreateArea.jsx
+++ src/components/CreateArea.jsx
@@ -1,11 +1,32 @@
-import React from 'react'
+import React, { useState } from 'react'
 
-function CreateArea() {
+function CreateArea(params) {
+  const [inputTitle, setInputTitle] = useState('')
+  const [inputSubTitle, setInputSubTitle] = useState('')
+  function handleChangeTitle(params) {
+    const newInputTitle = params.target.value
+    setInputTitle(newInputTitle)
+  }
+  function handleChangeSubTitle(params) {
+    const newInputSubTitle = params.target.value
+    setInputSubTitle(newInputSubTitle)
+  }
   return (
     <div>
       <form>
-        <input name="title" placeholder="Title" />
-        <textarea name="content" placeholder="Take a note..." rows="3" />
+        <input
+          onChange={handleChangeTitle}
+          name="title"
+          value={inputTitle}
+          placeholder="Title"
+        />
+        <textarea
+          onChange={handleChangeSubTitle}
+          name="content"
+          value={inputSubTitle}
+          placeholder="Take a note..."
+          rows="3"
+        />
         <button>Add</button>
       </form>
     </div>
